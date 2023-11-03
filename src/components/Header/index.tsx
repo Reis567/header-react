37cc13e58c824bee0a8dd769f23a05c673dd7fac
Reis567 <matheusdosreis9@gmail.com>
2023-11-03 07:30:51 -0300

    creation of header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css'
+
+const Header: React.FC = () =>{
+    return(
+        <header>
+            <a href="https://github.com/Reis567" target='_blank'rel="noreferrer">
+                <h1>
+                    Reis567
+                </h1>
+            </a>
+        </header>
+    )
+}
+export default Header
